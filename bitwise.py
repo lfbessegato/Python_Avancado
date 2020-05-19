343605e8015b7f0a0475b63f8b73caf3f2dd3bf1
Luciano Bessegato <lfrbessegato@gmail.com>
2020-05-19 16:18:18 -0300

    Bitwise - bit a bit

---
+++ bitwise.py
@@ -0,0 +1,18 @@
+# bitwise -> operadores bit a bit
+n1 = 10
+print(bin(n1))
+
+n2 = 5
+print(bin(n2))
+
+# operador AND
+r = n1 & n2
+print(r, bin(r))
+
+# operador OR
+r = n1 | n2
+print(r, bin(r))
+
+# operador XOR
+r = n1 ^ n2
+print(r, bin(r))
