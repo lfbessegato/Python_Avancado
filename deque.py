639064b550bc95cdbb2eb17c81e8e973e9b3ce30
Luciano Bessegato <lfrbessegato@gmail.com>
2020-05-19 15:28:27 -0300

    Deque

---
+++ deque.py
@@ -0,0 +1,36 @@
+# deque -> Acronimo de fila de duas pontas
+# estrutura que pode remover do inicio ou do final da fila (FIFO)
+from collections import deque
+
+'''
+fila = deque(maxlen=4)
+
+fila.append(1)
+
+fila.append(2)
+
+fila.append(3)
+
+fila.append(4)
+
+fila.append(5)
+
+print(fila)
+'''
+f = deque()
+
+f.append(1)
+
+f.append(2)
+
+f.append(3)
+
+f.appendleft(4)
+
+print(f)
+
+f.pop()
+
+f.popleft()
+
+print(f)
