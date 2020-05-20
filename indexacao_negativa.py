9ee86a8f50376c79925b64a6ddbf7de4a942556e
Luciano Bessegato <lfrbessegato@gmail.com>
2020-05-20 14:59:49 -0300

    Indexação Negativa

---
+++ indexacao_negativa.py
@@ -0,0 +1,14 @@
+lista = [1, 2, 3, 4, 5, 6]
+print(lista)
+
+# Último Elemento
+print(lista[-1])
+
+# Penúltimo Elemento
+print(lista[-2])
+
+# Slice com indexação negativa
+print(lista[-4:-2])
+
+# Com passo negativo
+print(lista[::-2])
