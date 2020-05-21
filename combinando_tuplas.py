2f9a6221a8bd79c6262db069d0ad01400b485112
Luciano Bessegato <lfrbessegato@gmail.com>
2020-05-21 14:39:57 -0300

    Combinando elementos em tuplas

---
+++ combinando_tuplas.py
@@ -0,0 +1,18 @@
+# utilizando a função zip
+r = zip([1,2,3], [1,2])
+
+# mostra o objeto zip
+print(f'Objeto: {r}')
+
+# Combinando as tuplas
+print(f'Combinando as Listas com o ZIP: {list(r)}')
+
+
+# utilizando outro exemplo com a função zip
+r = zip([1,2,3], [4,5,6])
+
+# mostra o objeto zip
+print(f'Objeto: {r}')
+
+# Combinando as tuplas
+print(f'Combinando as Listas com o ZIP - exemplo2: {list(r)}')
