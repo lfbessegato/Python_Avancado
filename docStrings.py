f4f886c7df4a169a149ab5239b5401525bcbb5a5
Luciano Bessegato <lfrbessegato@gmail.com>
2020-05-26 14:34:04 -0300

    Aula 54 - DocStrings

---
+++ docStrings.py
@@ -0,0 +1,7 @@
+def multiplicar(x, y):
+    """ Funcao multiplicar que retorna a multiplicacaoo de dois numeros """
+
+    return x * y
+
+# O texto será apresentando quando for executado o comando help da função
+help(multiplicar)
