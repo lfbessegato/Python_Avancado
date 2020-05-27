04a38f5e55a46f2686e9759cb016428a1a664213
Luciano Bessegato <lfrbessegato@gmail.com>
2020-05-27 16:01:15 -0300

    Aula 61 - Lancando Excecoes

---
+++ excecoes.py
@@ -0,0 +1,15 @@
+class SomentePares(list):
+	def append(self, inteiro):
+		if not isinstance(inteiro, int):
+
+			raise TypeError('Somente inteiros')
+
+		if inteiro % 2:
+			raise ValueError('Somente pares')
+		super().append(inteiro)
+
+sp = SomentePares()
+sp.append(10)
+sp.append(20)
+sp.append(4)
+print(sp)
