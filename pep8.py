296ffef6efe1449bc8c756a86178bea1d905861e
Luciano Bessegato <lfrbessegato@gmail.com>
2020-05-27 16:39:02 -0300

    Aula 63 - PEP 8

---
+++ pep8.py
@@ -0,0 +1,25 @@
+# PEP8 - Guia de estilo para o código Python
+
+import os
+import sys
+
+r = minha_funcao(var_one, var_two, 
+                 var_three, var_four)
+
+my_list = [
+    1, 2, 3,
+    4, 5, 6,
+    ]
+
+result = (var1
+	      + var2
+	      + (var3 - var4)
+	      - result_var1
+	      - result_var4)
+
+#foo( x, y ) // espaços extras
+
+foo(x, y)
+if x == 4: print(x)
+
+#calcular_media
