2d0e6dc0f48c09ba207ae24dfcb7af307396cf0b
Luciano Bessegato <lfrbessegato@gmail.com>
2020-05-29 16:17:09 -0300

    Design Patterns - Aula 78 instanciacao preguicosa no padrao Singleton

---
+++ Design Patterns/instanciaSingleton.py
@@ -0,0 +1,17 @@
+class Singleton:
+	__instance = None
+	def __init__(self):
+		if not Singleton.__instance:
+			print('__init__ foi chamado')
+		else:
+			print('instancia ja criada: ', self.obter_instancia())
+
+	@classmethod
+	def obter_instancia(cls):
+		if not cls.__instance:
+			cls.__instance = Singleton()
+		return cls.__instance
+
+s = Singleton()
+print('Objeto criado: ', Singleton.obter_instancia())
+s2 = Singleton()
