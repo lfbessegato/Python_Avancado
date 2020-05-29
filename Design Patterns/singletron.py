b3d8d825da5c7882fb284f9e768780c032e6aaae
Luciano Bessegato <lfrbessegato@gmail.com>
2020-05-29 16:10:18 -0300

    Design Patterns - Aula 77 implantacao do padrao Singleton

---
+++ Design Patterns/singletron.py
@@ -0,0 +1,14 @@
+'''
+Padrão de projeto Singleton
+'''
+
+class Singleton(object):
+	def __new__(cls):
+		if not hasattr(cls, 'instance'):
+			cls.instance = super(Singleton, cls).__new__(cls)
+		return cls.instance
+
+s1 = Singleton()
+print(s1)
+s2 = Singleton()
+print(s2)
